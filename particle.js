b4a9780cd169a3598e0ae92c73c12e18f24436a8
HOANG THINH TRAN <113574878+HoangThinhTran@users.noreply.github.com>
2024-04-10 12:36:40 -0700

    Update particle.js

--- particle.js
+++ particle.js
@@ -5,7 +5,12 @@ class Particle extends VerletParticle2D {
         physics.addParticle(this);
     }
     show() {
-        fill(0);
+        fill(100);
+        stroke(0);
         circle(this.x, this.y, this.r * 2);
+        let num = [];
+        for(let i = 1; i < 4; i++) {
+            text(num[i], this.x, this.y);
+        }
     }
 }
